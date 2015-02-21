7794b097f8eee8fd810c919990a186acacc35b61
Luyet Gil <gil.luyet@unifr.ch>
2015-02-21 08:45:20 +0100

    moving platforms

--- python/platformer/empty_platformer.py
+++ python/platformer/empty_platformer.py
@@ -60,6 +60,8 @@ class Player(pygame.sprite.Sprite):
         # List of sprites we can bump against
         self.level = None
 
+        self.status = 'stand' #stand,moving
+
     def update(self):
         """ Move the player. """
         # Gravity
@@ -120,19 +122,20 @@ class Player(pygame.sprite.Sprite):
         # If it is ok to jump, set our speed upwards
         if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
             self.change_y = -10
-
+            self.status = 'moving'
     # Player-controlled movement:
     def go_left(self):
         """ Called when the user hits the left arrow. """
-        self.change_x = -6
-
+        self.change_x += -6
+        self.status = 'moving'
     def go_right(self):
         """ Called when the user hits the right arrow. """
-        self.change_x = 6
-
+        self.change_x += 6
+        self.status = 'moving'
     def stop(self):
         """ Called when the user lets off the keyboard. """
         self.change_x = 0
+        self.status = 'stand'
 
 class Platform(pygame.sprite.Sprite):
     """ Platform the user can jump on """
@@ -145,6 +148,83 @@ class Platform(pygame.sprite.Sprite):
         self.image.fill(GREEN)
 
         self.rect = self.image.get_rect()
+
+class MovingPlatform(Platform):
+    """ This is a fancier platform that can actually move. """
+    change_x = 0
+    change_y = 0
+
+    boundary_top = 0
+    boundary_bottom = 0
+    boundary_left = 0
+    boundary_right = 0
+
+    player = None
+
+    level = None
+
+    def update(self):
+        """ Move the platform.
+            If the player is in the way, it will shove the player
+            out of the way. This does NOT handle what happens if a
+            platform shoves a player into another object. Make sure
+            moving platforms have clearance to push the player around
+            or add code to handle what happens if they don't. """
+
+        # Move left/right
+        self.rect.x += self.change_x
+
+        # See if we hit the player
+        hit = pygame.sprite.collide_rect(self, self.player)
+
+        if hit:
+            # We did hit the player. Shove the player around and
+            # assume he/she won't hit anything else.
+
+            # If we are moving right, set our right side
+            # to the left side of the item we hit
+            if self.change_x < 0:
+                self.player.rect.right = self.rect.left
+            else:
+                # Otherwise if we are moving left, do the opposite.
+                self.player.rect.left = self.rect.right
+
+        # Move up/down
+        self.rect.y += self.change_y
+
+        # Check and see if we hit the player
+        hit = pygame.sprite.collide_rect(self, self.player)
+
+        if hit:
+            # We did hit the player. Shove the player around and
+            # assume he/she won't hit anything else.
+
+            # Reset our position based on the top/bottom of the object.
+            if self.change_y < 0:
+                self.player.rect.bottom = self.rect.top
+            else:
+                self.player.rect.top = self.rect.bottom
+
+        #check if the player is on top of the movingplatform and move it with it.
+
+        #check if the player is on top of a lateral moving platform and make it move with it.
+        self.player.rect.y += 1
+        hit = pygame.sprite.collide_rect(self.player, self)
+        self.player.rect.y -= 1
+
+        if hit:
+            if self.player.status == 'stand':
+                self.player.change_x = self.change_x
+
+        # Check the boundaries and see if we need to reverse
+        # direction.
+        if self.rect.bottom > self.boundary_bottom or self.rect.top < self.boundary_top:
+            self.change_y *= -1
+
+        cur_pos = self.rect.x - self.level.world_shift
+        if cur_pos < self.boundary_left or cur_pos > self.boundary_right:
+            self.change_x *= -1
+
 
 class Level():
     """ This is a generic super-class used to define a level.
@@ -205,7 +285,7 @@ class Level_01(Level):
         # Call the parent constructor
         Level.__init__(self, player)
 
-        self.level_limit = -1000
+        self.level_limit = -1500
 
         # Array with width, height, x, and y of platform
         level = [[210, 70, 500, 500],
@@ -222,6 +302,17 @@ class Level_01(Level):
             block.rect.y = platform[3]
             block.player = self.player
             self.platform_list.add(block)
+
+        # Add a custom moving platform
+        block = MovingPlatform(70, 40)
+        block.rect.x = 1350
+        block.rect.y = 380
+        block.boundary_left = 1350
+        block.boundary_right = 1600
+        block.change_x = 1
+        block.player = self.player
+        block.level = self
+        self.platform_list.add(block)
 
 # Create platforms for the level
 class Level_02(Level):
@@ -259,7 +350,7 @@ def main():
     size = [SCREEN_WIDTH, SCREEN_HEIGHT]
     screen = pygame.display.set_mode(size)
 
-    pygame.display.set_caption("Side-scrolling Platformer")
+    pygame.display.set_caption("Empty Platformer")
 
     # Create the player
     player = Player()
@@ -305,6 +396,8 @@ def main():
                     player.stop()
                 if event.key == pygame.K_RIGHT and player.change_x > 0:
                     player.stop()
+
+
 
         # Update the player.
         active_sprite_list.update()
