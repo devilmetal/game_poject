3d8ac22df4b7bbcb3f00e21f095f2a42f277ec9a
Luyet Gil <gil.luyet@unifr.ch>
2015-03-20 13:46:28 +0100

    adding automatic sacling for character selection

--- python/game/CharacterMenu.py
+++ python/game/CharacterMenu.py
@@ -15,9 +15,21 @@ class CharacterMenu:
         self.screen=screen
         bob = routines.load_png('hero/idle_l.png')
         hulk = routines.load_png('hero_1/idle_l.png')
-        self.characters = [bob,hulk]
+        hulk1 = routines.load_png('hero_1/idle_l.png')
+        bob2 = routines.load_png('hero/idle_l.png')
+
+
+        #self.characters = [bob,hulk]
+        self.characters = [bob,hulk,hulk1,bob2]
         self.draw(0)
 
+    def fib(self,n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fib(n-1) + self.fib(n-2)
 
     #Used inside the run() function with events.
     def draw_by_hop(self,menu_hop):
@@ -57,6 +69,7 @@ class CharacterMenu:
         image_width=40
         x_deviation = int(constants.SCREEN_WIDTH/(len(self.characters)+1))
         y_position = constants.SCREEN_HEIGHT/2
+        scale_tail = self.fib(len((self.characters)))
         #Draw all figures
         for character in self.characters:
             #Compute position
@@ -65,10 +78,10 @@ class CharacterMenu:
             character[1].x = x_position
             character[1].y = y_position
             #Rescale image
-            character_scale = pygame.transform.scale(character[0], (character[1].width*2, character[1].height*2))
+            character_scale = pygame.transform.scale(character[0], (int(float(character[1].width)*float(2.0/scale_tail)), int(float(character[1].height)*float(2.0/scale_tail))))
             #Draw background rect of selected figure according to self.selected
             if position == self.selected:
-                back = pygame.Surface([character[1].width*2+40, character[1].height*2+40])
+                back = pygame.Surface([int(float(character[1].width)*float(2.0/scale_tail))+40, int(float(character[1].height)*float(2.0/scale_tail))+40])
                 back.fill((153,102,255))
                 back_rect = character[1].copy()
                 back_rect.x-=20
@@ -92,18 +105,18 @@ class CharacterMenu:
                 if event.type == pygame.JOYHATMOTION:
                     hat = joystick.get_hat(0)
                     if (1, 0) == hat:
-                        self.draw_by_hop(-1)
-                    if (-1, 0) == hat:
                         self.draw_by_hop(1)
+                    if (-1, 0) == hat:
+                        self.draw_by_hop(-1)
                 if event.type == pygame.JOYBUTTONDOWN:
                     if joystick.get_button(1) == 1:
                         return self.selected
                 #Keyboard stuff
                 if event.type == pygame.KEYDOWN: # or event.type == pygame.JOYHATMOTION or event.type == pygame.JOYBUTTONDOWN:
                     if event.key == pygame.K_RIGHT:# or joystick.get_hat()==(0,1):
-                        self.draw_by_hop(-1) #here is the Menu class function
-                    if event.key == pygame.K_LEFT:# or joystick.get_hat()==(0,-1):
                         self.draw_by_hop(1) #here is the Menu class function
+                    if event.key == pygame.K_LEFT:# or joystick.get_hat()==(0,-1):
+                        self.draw_by_hop(-1) #here is the Menu class function
                     if event.key == pygame.K_RETURN:
                         return self.selected
                 elif event.type == pygame.QUIT:
