f34f9bd8a71d61b08e36a22e1d35db6b5a1c753d
Luyet Gil <gil.luyet@unifr.ch>
2015-03-25 13:41:27 +0100

    correction bug moving dead character

--- python/game/characters/Character.py
+++ python/game/characters/Character.py
@@ -84,6 +84,7 @@ class Character(pygame.sprite.Sprite):
                 self.change_y = 0
         else:
             self.rect.y += self.change_y
+            self.change_x = 0
 
 
         #Finally calculate the image to display
