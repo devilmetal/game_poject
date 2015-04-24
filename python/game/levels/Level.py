7f9c93de0c8e37d48c1663ecba8c06cbf9ca0e11
Luyet Gil <gil.luyet@unifr.ch>
2015-04-24 13:10:03 +0200

    optimiz

--- python/game/levels/Level.py
+++ python/game/levels/Level.py
@@ -49,20 +49,25 @@ class Level():
         # Draw all the sprite lists that we have
         #self.back_world_list.draw(screen)
         for elem in self.back_world_list:
-            screen.blit(elem.image,elem.rect)
+            if not(screen.get_rect().collidelist([elem])):
+                screen.blit(elem.image,elem.rect)
         #self.back_front_world_list.draw(screen)
         for elem in self.back_front_world_list:
-            screen.blit(elem.image,elem.rect)
+            if not(screen.get_rect().collidelist([elem])):
+                screen.blit(elem.image,elem.rect)
         #self.platform_list.draw(screen)
         for elem in self.platform_list:
-            screen.blit(elem.image,elem.rect)
+            if not(screen.get_rect().collidelist([elem])):
+                screen.blit(elem.image,elem.rect)
         #pygame.sprite.spritecollide(screen, self.platform_list, False).draw(screen)
         #self.magma_list.draw(screen)
         for elem in self.magma_list:
-            screen.blit(elem.image,elem.rect)
+            if not(screen.get_rect().collidelist([elem])):
+                screen.blit(elem.image,elem.rect)
         #self.pnj_list.draw(screen)
         for elem in self.pnj_list:
-            screen.blit(elem.image,elem.rect)
+            if not(screen.get_rect().collidelist([elem])):
+                screen.blit(elem.image,elem.rect)
         self.player.interface.draw(screen, self.player.lives)
 
     def shift_world(self, shift_x):
