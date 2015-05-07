efde529d486f0c7920e65d43e21d0b16c461b621
nad0u <ae.nadia@gmail.com>
2015-05-07 21:51:35 +0200

    changed platforms colors (grass)

--- python/game/constants.py
+++ python/game/constants.py
@@ -7,7 +7,8 @@ BLUE     = (   0,   0, 255)
 RED      = ( 255,   0,   0)
 GREEN    = (   0, 255,   0)
 SKY_BLUE = ( 179, 232, 255)
-PLAT_COL = ( 102,  51,   0)
+# PLAT_COL = ( 102,  51,   0)
+PLAT_COL = (60,56,39)
 # Screen dimensions
 SCREEN_WIDTH  = 800
 SCREEN_HEIGHT = 500
